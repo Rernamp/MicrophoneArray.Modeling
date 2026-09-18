17834affc7eadc2910106cd71613c64d654a205e
rernamp <renamp100@gmail.com>
2026-09-18 07:52:18 +0300

    del state for link number with slider, because slider contain this functional

---
+++ modeling/planarArray.py
@@ -0,0 +1,22 @@
+import marimo
+
+__generated_with = "0.23.15"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    import marimo as mo
+    import numpy as np
+
+    return
+
+
+@app.cell
+def _():
+    number_elements = 4
+    return
+
+
+if __name__ == "__main__":
+    app.run()
